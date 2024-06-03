38b3b81d31e73b1c0abea7722a7ef62b7a58c94a
lousydropout <lousydropout@gmail.com>
2024-06-02 17:07:35 -0700

    add network info for avalanch testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,12 +4,17 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
-  defaultNetwork: "moonbase",
+  defaultNetwork: "moonbaseAlpha",
   networks: {
-    moonbase: {
+    moonbaseAlpha: {
       url: "https://moonbase-alpha.public.blastapi.io", // RPC URL for Moonbase Alpha
       accounts: [`0x${process.env.PRIVATE_KEY}`], // Your private key
       chainId: 1287, // Chain ID for Moonbase Alpha
+    },
+    avalancheTestnet: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Your private key
+      chainId: 43113, // Chain ID for Avalanche Testnet
     },
   },
 };
